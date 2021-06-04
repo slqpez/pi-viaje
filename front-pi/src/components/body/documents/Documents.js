f3b4e1f88b2d2748e8ddb63ce763836def6c657f
slqpez <santiago.lopez16@udea.edu.co>
2021-06-04 13:31:07 -0500

    Creating uploadDocument function.

--- front-pi/src/components/body/documents/Documents.js
+++ front-pi/src/components/body/documents/Documents.js
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react'
 import "./documents.css"
+
+
+
 function Documents() {
 
 
-    const uploadDocument =()=>{
+    const [name, setName] = useState("")
+    const [selectedFile, setSelectedFile] = useState(null);
 
+    const handleName =(e)=>{
+        setName(e.target.value)
+    }
+    const handleFile=(e)=>{
+        setSelectedFile(e.target.files[0])
     }
 
-    const addDocument=()=>{
-        const documentsDiv = document.querySelector(".documents")
-        documentsDiv.innerHTML += `
-        <div className="document-to-upload">
-        <input
-            type="file"
-            name="file"
-            id="doc_up"
-            onChange={uploadDocument}
-        ></input>
-        <input type="text" className="document-name"></input>
-     </div>
-        `
+    const addDocument=(e)=>{
+        e.preventDefault();
+        /* try { */
+          ;
+          console.log(selectedFile)
+          /* if (!file)
+            return setData({
+              ...data,
+              err: "No se subió el archivo.",
+              success: "",
+            });
+    
+          if (file.size > 1024 * 1024) {
+            return setData({
+              ...data,
+              err: "El archivo es demasiado grande.",
+              success: "",
+            });
+          }
+    
+          if (file.type !== "image/jpeg" && file.type !== "image/png")
+            return setData({
+              ...data,
+              err: "El formato del archivo es inválido.",
+              success: "",
+            });
+     */
+          /* let formData = new FormData();
+          formData.append("file", file); */
+    
+          /* setLoading(true);
+          const res = await axios.post(
+            "https://pi-gest-viaje.herokuapp.com/api/upload_avatar",
+            formData,
+            {
+              headers: {
+                "content-type": "multipart/form-data",
+                Authorization: token,
+              },
+            }
+          );
+    
+          setLoading(false);
+          setAvatar(res.data.url);
+        } catch (err) {
+          setData({ ...data, err: err.response.data.msg, success: "" });
+        } */
+       
     }
     return (
         <div className="documents-page">
@@ -30,15 +74,16 @@ function Documents() {
                         type="file"
                         name="file"
                         id="doc_up"
-                        onChange={uploadDocument}
+                        onChange={handleFile}
+                        
                     ></input>
-                    <input type="text" className="document-name"></input>
+                    <input type="text"  onChange={(e)=> handleName(e)} value={name} className="document-name"></input>
                  </div>
            
 
             </div>
            
-              <button onClick={addDocument}>Agregar otro documento ➕</button>
+              <button onClick={addDocument}>Agregar documento</button>
         </div>
     )
 }
